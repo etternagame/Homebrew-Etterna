2f77fc282d26f0451d5f530efac18dbb01e6b641
Andrew Plotner <bluebandit21@gmail.com>
2024-03-31 16:37:07 -0400

    Attempt 4

--- Formula/o/openssl@1.1_osx_high_sierra.rb
+++ Formula/o/openssl@1.1_osx_high_sierra.rb
@@ -1,4 +1,4 @@
-class OpensslAT11 < Formula
+class OpensslAT11OSXHighSierra < Formula
   desc "Cryptography and SSL/TLS Toolkit"
   homepage "https://openssl.org/"
   url "https://www.openssl.org/source/openssl-1.1.1w.tar.gz"
